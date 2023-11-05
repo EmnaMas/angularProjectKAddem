66518925d6b602bc13b442eec1890c50e1dae3d1
EmnaMas <98929299+EmnaMas@users.noreply.github.com>
2023-11-05 16:35:11 +0100

    first commit

---
+++ Kaddem_FrontEnd-master/src/app/Services/departments.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import baseUrl from './Helpers';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentsService {
+
+  constructor(private http : HttpClient) { }
+
+  public getDepartments(){
+
+    return this.http.get(`${baseUrl}/departement`);
+    
+
+  }
+
+
+  public addDepartment(depatement:any){
+
+    return this.http.post(`${baseUrl}/departement/add/`,depatement);
+
+  }
+
+  updateDepartment(depatement:any){
+  
+    return this.http.put(`${baseUrl}/departement/update`, depatement);
+
+  }
+
+  deleteDepartment(id:any){
+
+    return this.http.delete(`${baseUrl}/departement/delete/${id}`);
+  }
+
+
+
+  getDepartment(id:any){
+
+    return this.http.get(`${baseUrl}/departement/${id}`);
+
+  }
+
+
+  getCountDept(){
+
+    return this.http.get(`${baseUrl}/departement/count`);
+
+
+  }
+
+
+
+
+}
---
+++ Kaddem_FrontEnd-master/src/app/profile/profile.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { EtudiantService } from '../Services/etudiant.service';
+import Swal from 'sweetalert2';
+import { LoginService } from '../Services/login.service';
+import { EquipeService } from '../Services/equipe-service';
+import { DepartmentsService } from '../Services/departments.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+
+  nbEtudiant !:number;
+  nbEquipe !:number;
+  nbDept !:number;
+
+
+
+  user :any = null;
+
+  constructor(private etudiant : EtudiantService, private login: LoginService, private equipe : EquipeService
+    
+    , private dept : DepartmentsService) { }
+
+  ngOnInit(): void {
+     this.countEtudiant();
+     this.countEquipe();
+     this.countDept();
+
+    this.user = this.login.getUser();
+  }
+
+
+  countEtudiant(){
+
+    this.etudiant.getCountEtudiant().subscribe(
+      (data:any)=>{
+        this.nbEtudiant = data;
+        //console.log(this.nbEtudiant);
+      },
+      (error)=>{
+        console.log(error);
+        Swal.fire('Erreur !!', 'Erreur dans la récupération des deonnées', 'error');
+      }
+    )
+  };
+
+
+  countEquipe(){
+    this.equipe.getCountEquipe().subscribe(
+      (data:any)=>{
+        this.nbEquipe = data;
+       
+      },
+      (error)=>{
+        console.log(error);
+        Swal.fire('Erreur !!', 'Erreur dans la récupération des deonnées', 'error');
+      }
+    )
+  };
+
+
+
+  countDept(){
+    this.dept.getCountDept().subscribe(
+      (data:any)=>{
+        this.nbDept = data;
+
+      },
+      (error)=>{
+        console.log(error);
+        Swal.fire('Erreur !!', 'Erreur dans la récupération des deonnées', 'error');
+      }
+    )
+  };
+
+}
